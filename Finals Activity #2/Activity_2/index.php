dc1e7700e194de52445079bf9b08eecba5327f69
joshuaobis15 <obisjoshua3@gmail.com>
2026-04-23 21:14:21 +0800

    Add index.php with multiple PHP outputs

---
+++ Finals Activity #2/Activity_2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiple Outputs</title>
+</head>
+
+<body>
+
+    <?php
+    echo "What's up madlang people!<br>";
+    echo "Ang buhay ay weather weather lang.<br>";
+    echo "Ako Kuya si Kim Matang Lawin!";
+    ?>
+
+</body>
+
+</html>
