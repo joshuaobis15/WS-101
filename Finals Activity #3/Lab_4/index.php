b751bf4581ff2f6a2a98283f0271fe2cef29d95c
joshuaobis15 <obisjoshua3@gmail.com>
2026-05-13 05:05:11 +0800

    Modify output to remove htmlspecialchars for inputs
    Removed htmlspecialchars from output for name and email.

--- Finals Activity #3/Lab_4/index.php
+++ Finals Activity #3/Lab_4/index.php
@@ -23,8 +23,8 @@
     if (isset($_POST['name']) && isset($_POST['email'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
-        echo "Name: " . htmlspecialchars($name) . "<br>";
-        echo "Email: " . htmlspecialchars($email);
+        echo "Name: " . ($name) . "<br>";
+        echo "Email: " . ($email);
     }
 
     ?>
